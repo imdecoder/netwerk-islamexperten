2be0ddc51a879c97e4d299257030b43d989bbce9
Demirsatan Emre <59002285@esfds.net>
2020-07-15 12:52:30 +0200

    workshops page

--- src/app/pages/workshops/workshops.page.ts
+++ src/app/pages/workshops/workshops.page.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { WordpressService } from 'src/app/services/wordpress.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-workshops',
   templateUrl: './workshops.page.html',
   styleUrls: ['./workshops.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WorkshopsPage implements OnInit {
   content: any;
 
-  constructor(private wp: WordpressService) { }
+  constructor(
+    private wp: WordpressService,
+    private sanitizer: DomSanitizer
+    ) { }
 
   ngOnInit() {
     this.wp.getPage(254).subscribe(res => {
-      this.content = res;
+      this.content = this.sanitizer.bypassSecurityTrustHtml(res['content']['rendered']);
     });
+  }
+
+  ionViewDidLoad(){
+    console.log("loaddddd");
   }
 
 }
